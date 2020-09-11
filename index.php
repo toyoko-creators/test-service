d85fc04433041845b059e460ff9e6bb89e85994e
toyoko.creators <toyoko.creators@gmail.com>
2020-09-11 08:03:46 +0000

    login

--- index.php
+++ index.php
@@ -12,3 +12,17 @@
  */
 echo "Hello World!rrrrr"
 ?>
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<title>てすと</title>
+</head>
+<body>
+	<form id="Rogin" action="">
+		<p><label>ログインID(メールアドレス)<input type="text" name="ID" size="40"></label></p>
+		<p><label>ログインパスワード(英数字8-20文字)<input type="password" name="PASSWORD" size="40"  maxlength="20"></label></p>
+		 <label>IDを保存する<input type="checkbox"></label><br>
+		<input type="submit"value="ログイン"> <input type="button"value="ID,パスワードを忘れた場合はこちら">
+	</form>
+</body>
+</html>
