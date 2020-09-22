d4fd763f62b6727a7ecc82794862504047591592
toyoko.creators <toyoko.creators@gmail.com>
2020-09-22 18:53:50 +0900

    デザイン更新

--- top.php
+++ top.php
@@ -23,16 +23,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=device-width">
     <title>トップ画面</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
  
 <body>
-<h1>トップ画面</h1>
-<p><?php echo $_SESSION['USER'] ?>さんでログイン中</p>
-<br>
-<form method="post" action="top.php">
-    <input type="submit" name="logout" value="ログアウト">
-</form>
- 
+<div class="form-wrapper">
+    <h1>トップ画面</h1>
+    
+    <form method="post" action="top.php">
+        <p align="center"><?php echo $_SESSION['USER'] ?>さんでログイン中</p>
+    
+        <div class="button-panel">
+            <input type="submit" class="button" name="logout" value="ログアウト">
+        </div>
+    </form>
+    <div class="form-footer"></div>
+</div>
 </body>
 </html>
