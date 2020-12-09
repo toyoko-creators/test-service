57f959ca6beca0a28ac4ffb2921267167e781012
toyoko.creators <toyoko.creators@gmail.com>
2020-12-09 23:27:57 +0900

    ・上下の服の選択状態をそれぞれ、保持するために、slickのクラスをわけた。 ・選択した画像を中央表示にしているので、slick-centerでborderを指定し、中央の画像に赤枠を表示 ・chromeでデフォルトのフォーカスに黒がつくのをcssで無効化

--- public/js/common.js
+++ public/js/common.js
@@ -2,7 +2,7 @@ $('.slick01').slick(
 {
     centerMode: true,
     centerPadding: '20%',
-    dots: true,
+    // dots: true,
     variableWidth: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -11,3 +11,15 @@ $('.slick01').slick(
     
   });
 
+  $('.slick02').slick(
+    {
+        centerMode: true,
+        centerPadding: '20%',
+        // dots: true,
+        variableWidth: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,      // スライドをループしない
+        focusOnSelect: true,  // 中央以外をクリックすると、中央まで移動(=フォーカス)する。
+        
+    });
